dc81ed8ebd2a6ce9ee8826668efa028a6c0dd4e3
Priya <priyas.fcs@gmail.com>
2020-01-14 13:14:53 -0500

    Re arrange

--- src/object_tracking.py
+++ src/object_tracking.py
@@ -5,20 +5,10 @@ import argparse
 import imutils
 import time
 import cv2
+import sys
+import glob
 import serial
 from time import sleep
-
-# open Arduino Serial
-ser1 = serial.Serial('COM3', 9600)
-sleep(2)
-
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", type=str,
-                help="path to input video file")
-ap.add_argument("-t", "--tracker", type=str, default="csrt",
-                help="OpenCV object tracker type")
-args = vars(ap.parse_args())
 
 # define stuff for x-coordinate detection
 ovcenterx = 0
@@ -37,37 +27,74 @@ newydirection = 'N'
 centery = 0
 
 # flipping variables
+portopen = bool(False)
 horizder = bool(False)
 vertder = bool(False)
 rotate = bool(False)
 sendrepeat = bool(False)
 centered = bool(False)
 
-xrangeh = 55
-xrangel = 45
-yrangeh = 60
-yrangel = 40
+# range limits
+xrangeh = 65
+xrangel = 35
+yrangeh = 65
+yrangel = 35
+availvid = []
 
+# check for available serial ports
+def serial_ports():
+    # List serial ports
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(50)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
 
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+# open Arduino Serial
+availableport = serial_ports()
+if len(availableport) > 0:
+    ser1 = serial.Serial(availableport[0], 9600)
+    sleep(2)
+    portopen = True
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", type=str,
+                help="path to input video file")
+ap.add_argument("-t", "--tracker", type=str, default="csrt",
+                help="OpenCV object tracker type")
+args = vars(ap.parse_args())
+
+# sends command to the Arduino over serial port
 def sendCommand(cmd):
-    ser1.write(cmd)
-    s = ser1.readline();
-    if not centered:
+    if portopen:
         ser1.write(cmd)
-    # print("arduino sent back -> " + s.decode())
+        s = ser1.readline();
+        if not centered:
+            ser1.write(cmd)
+        # print("arduino sent back -> " + s.decode())
 
 # extract the OpenCV version info
 (major, minor) = cv2.__version__.split(".")[:2]
 
-# if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
 # function to create our object tracker
 if int(major) == 3 and int(minor) < 3:
     tracker = cv2.Tracker_create(args["tracker"].upper())
-
-# otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
-# approrpiate object tracker constructor:
 else:
-    # initialize a dictionary that maps strings to their corresponding
     # OpenCV object tracker implementations
     OPENCV_OBJECT_TRACKERS = {
         "csrt": cv2.TrackerCSRT_create,
@@ -78,19 +105,28 @@ else:
         "medianflow": cv2.TrackerMedianFlow_create,
         "mosse": cv2.TrackerMOSSE_create
     }
-
-    # grab the appropriate object tracker using our dictionary of
     # OpenCV object tracker objects
     tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
 
-# initialize the bounding box coordinates of the object we are going
-# to track
+# initialize the bounding box coordinates
 initBB = None
+
+# Checks for a valid camera
+def testDevice(source):
+   cap = cv2.VideoCapture(source)
+   if cap is None or not cap.isOpened():
+       return False
+   else:
+       return True
+
 
 # if a video path was not supplied, grab the reference to the web cam
 if not args.get("video", False):
     print("[INFO] starting video stream...")
-    vs = VideoStream(src=1).start()
+    for x in range(3):
+        if testDevice(x):
+            availvid.append(x)
+    vs = VideoStream(src=(availvid[len(availvid)-1])).start()
     time.sleep(1.0)
 
 # otherwise, grab a reference to the video file
@@ -99,149 +135,128 @@ else:
 
 # initialize the FPS throughput estimator
 fps = None
-print(xrangel, xrangeh, yrangel, yrangeh)
-# loop over frames from the video stream
-while True:
-    # grab the current frame, then handle if we are using a
-    # VideoStream or VideoCapture object
-    frame = vs.read()
-    frame = frame[1] if args.get("video", False) else frame
 
-    # check to see if we have reached the end of the stream
-    if frame is None:
-        break
+# defines how to make a move depending on location of bounding box center
+def makemove():
+    global smallx, largex, centerx, smally, largey, centery, newxdirection, oldxdirection, newydirection, oldydirection
+    smallx = x
+    largex = x + w
+    centerx = (smallx + largex) / 2
+    smally = y
+    largey = y + h
+    centery = (smally + largey) / 2
+    # Send X direction
+    if ((centerx / W) * 100) > xrangeh:
+        if horizder:
+            newxdirection = 'L'
+        else:
+            newxdirection = 'R'
+    elif ((centerx / W) * 100) < xrangel:
+        if horizder:
+            newxdirection = 'R'
+        else:
+            newxdirection = 'L'
+    else:
+        newxdirection = 'X'
+    if (oldxdirection != newxdirection) or sendrepeat:
+        sendCommand(newxdirection.encode())
+        oldxdirection = newxdirection
+    # Send Y direction
+    if ((centery / H) * 100) > yrangeh:
+        if vertder:
+            newydirection = 'D'
+        else:
+            newydirection = 'U'
+    elif ((centery / H) * 100) < yrangel:
+        if vertder:
+            newydirection = 'U'
+        else:
+            newydirection = 'D'
+    else:
+        newydirection = 'Y'
+    if (oldydirection != newydirection) or sendrepeat:
+        sendCommand(newydirection.encode())
+        oldydirection = newydirection
+    if (newydirection == 'Y') and (newxdirection == 'X'):
+        centered = True
+    else:
+        centered = False
+    return centered
 
-    # resize the frame (so we can process it faster) and grab the
-    # frame dimensions
-    frame = imutils.resize(frame, width=800)
-    (H, W) = frame.shape[:2]
-    ovcenterx = W/2
-
-    # check to see if we are currently tracking an object
-    if initBB is not None:
-        # grab the new bounding box coordinates of the object
-        (success, box) = tracker.update(frame)
-
-        # check to see if the tracking was a success
-        if success:
-            (x, y, w, h) = [int(v) for v in box]
-            smallx = x
-            largex = x + w
-            centerx = (smallx + largex)/2
-            smally = y
-            largey = y + h
-            centery = (smally + largey)/2
-
-            # Send X direction
-            if ((centerx / W) * 100) > xrangeh:
-                # print("out of bound - xh")
-                if horizder:
-                    newxdirection = 'L'
-                else:
-                    newxdirection = 'R'
-            elif ((centerx / W) * 100) < xrangel:
-                # print("out of bound - xl")
-                if horizder:
-                    newxdirection = 'R'
-                else:
-                    newxdirection = 'L'
-            else:
-                newxdirection = 'X'
-
-            if (oldxdirection != newxdirection) or sendrepeat:
-                sendCommand(newxdirection.encode())
-                oldxdirection = newxdirection
-                # print("Sent New X Direction")
-                # print(oldxdirection)
-
-            # Send Y direction
-            if ((centery / H) * 100) > yrangeh:
-                # print("out of bound - yh")
-                if vertder:
-                    newydirection = 'D'
-                else:
-                    newydirection = 'U'
-            elif ((centery / H) * 100) < yrangel:
-                # print("out of bound - yl")
-                if vertder:
-                    newydirection = 'U'
-                else:
-                    newydirection = 'D'
-            else:
-                newydirection = 'Y'
-
-            if (oldydirection != newydirection) or sendrepeat:
-                sendCommand(newydirection.encode())
-                oldydirection = newydirection
-                # print("Sent New Y Direction")
-                # print(oldydirection)
-
-            if (newydirection == 'Y') and (newxdirection == 'X'):
-                centered = True
-            else:
-                centered = False
-
-            cv2.rectangle(frame, (x, y), (x + w, y + h),
-                          (0, 255, 0), 2)
-
-        # update the FPS counter
-        fps.update()
-        fps.stop()
-
-        # initialize the set of information we'll be displaying on
-        # the frame
-        info = [
-            ("Success", "Yes" if success else "No"),
-            ("FPS", "{:.2f}".format(fps.fps())),
-            ("In Center", "Yes" if centered else "No"),
-        ]
-
-        # loop over the info tuples and draw them on our frame
-        for (i, (k, v)) in enumerate(info):
-            text = "{}: {}".format(k, v)
-            cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-
-    # show the output frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-
-    # print(key)
-
+# allows for the manual control of the platform
+def manualmove():
     # stop if space bar
     if key == ord("5"):
         sendCommand('S'.encode())
     if key == ord("8"):
+        print("Up/Down")
         if vertder:
             sendCommand('U'.encode())
         else:
             sendCommand('D'.encode())
     if key == ord("2"):
+        print("Up/Down")
         if vertder:
             sendCommand('D'.encode())
         else:
             sendCommand('U'.encode())
     if key == ord("4"):
+        print("Left/Right")
         if vertder:
             sendCommand('L'.encode())
         else:
             sendCommand('R'.encode())
     if key == ord("6"):
+        print("Left/Right")
         if vertder:
-            sendCommand('R'.encode())
+            sendCommand('qR'.encode())
         else:
             sendCommand('L'.encode())
 
-    # if the 's' key is selected, we are going to "select" a bounding
-    # box to track
+
+# loop over frames from the video stream
+while True:
+    frame = vs.read()
+    frame = frame[1] if args.get("video", False) else frame
+    if frame is None:
+        break
+    frame = imutils.resize(frame, width=800)
+    (H, W) = frame.shape[:2]
+    ovcenterx = W/2
+    # check to see if we are currently tracking an object
+    if initBB is not None:
+        # grab the new bounding box coordinates of the object
+        (success, box) = tracker.update(frame)
+        if success:
+            (x, y, w, h) = [int(v) for v in box]
+            centered = makemove()
+            cv2.rectangle(frame, (x, y), (x + w, y + h),
+                          (0, 255, 0), 2)
+        # update the FPS counter
+        fps.update()
+        fps.stop()
+
+        # initialize info on screen
+        info = [
+            ("Tracking Success", "Yes" if success else "No"),
+            ("X-Move", oldxdirection),
+            ("Y-Move", oldydirection),
+            ("In Center", "Yes" if centered else "No"),
+        ]
+        for (i, (k, v)) in enumerate(info):
+            text = "{}: {}".format(k, v)
+            cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    manualmove()
+
+    # if the 's' key is selected start tracking
     if key == ord("s"):
-        # select the bounding box of the object we want to track (make
-        # sure you press ENTER or SPACE after selecting the ROI)
         initBB = cv2.selectROI("Frame", frame, fromCenter=False,
                                showCrosshair=True)
-
         # start OpenCV object tracker using the supplied bounding box
-        # coordinates, then start the FPS throughput estimator as well
         tracker.init(frame, initBB)
         fps = FPS().start()
 
@@ -263,8 +278,6 @@ while True:
 sendCommand('S'.encode())
 if not args.get("video", False):
     vs.stop()
-
-# otherwise, release the file pointer
 else:
     vs.release()
 
