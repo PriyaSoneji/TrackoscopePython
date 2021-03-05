89fcdea542676951d71f10ca7dca71f5aab432a1
PriyaSoneji <priyas.fcs@gmail.com>
2021-03-05 11:15:07 -0500

    new graph testing

--- src/GraphTesting.py
+++ src/GraphTesting.py
@@ -19,20 +19,21 @@ x = data['x']
 y = data['y']
 
 timev = []
+timevadj = []
 mint = get_sec(df.min()[2])
 maxt = get_sec(df.max()[2])
 timediff = maxt - mint
 
 # converts the times to value between 0 and 255 so can do rgb
 for index, row in df.iterrows():
-    timev.append(round((255 / timediff) * ((get_sec(row['time'])) - mint), 2))
+    timev.append((get_sec(row['time'])) - mint)
+
+for index, row in df.iterrows():
+    timevadj.append(round((255 / timediff) * ((get_sec(row['time'])) - mint), 2))
 
 dydx = np.cos(0.5 * (x[:-1] + x[1:]))  # first derivative
 
 # Create a set of line segments so that we can color them individually
-# This creates the points as a N x 1 x 2 array so that we can stack points
-# together easily to get the segments. The segments array for line collection
-# needs to be (numlines) x (points per line) x 2 (for x and y)
 points = np.array([x, y]).T.reshape(-1, 1, 2)
 segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
@@ -40,22 +41,13 @@ fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111)
 
 # Create a continuous norm to map from data points to colors
-norm = plt.Normalize(dydx.min(), dydx.max())
+norm = plt.Normalize(timev[0], timev[-1])
 lc = LineCollection(segments, cmap='copper', norm=norm)
 # Set the values used for colormapping
-lc.set_array(dydx)
+lc.set_array(timevadj)
 lc.set_linewidth(2)
 line = ax.add_collection(lc)
 fig.colorbar(line, ax=ax)
-
-# Use a boundary norm instead
-# cmap = ListedColormap(['r', 'g', 'b'])
-# norm = BoundaryNorm([-1, -0.5, 0.5, 1], cmap.N)
-# lc = LineCollection(segments, cmap=cmap, norm=norm)
-# lc.set_array(dydx)
-# lc.set_linewidth(2)
-# line = ax.add_collection(lc)
-# fig.colorbar(line, ax=ax)
 
 ax.set_xlim(df.min()[0] - 500, df.max()[0] + 500)
 ax.set_ylim(df.min()[1] - 500, df.max()[1] + 500)
